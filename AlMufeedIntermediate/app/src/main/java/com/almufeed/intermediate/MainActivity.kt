88336e45ba9d41bff1e4eda81c01107583a2b2ba
alialawady912-svg <alialawady912@gmail.com>
2026-09-20 16:08:18 +0300

    Add files via upload

---
+++ AlMufeedIntermediate/app/src/main/java/com/almufeed/intermediate/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.almufeed.intermediate
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        // تفعيل خصائص الجافا سكريبت لتشغيل النظام بسلاسة
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.cacheMode = WebSettings.LOAD_DEFAULT
+
+        webView.webViewClient = WebViewClient()
+
+        // رابط موقع مدرسة المفيد المتوسطة الجديد
+        webView.loadUrl("https://mufeed-intermediate.web.app/")
+    }
+
+    // السماح بالرجوع للصفحات داخل التطبيق عند الضغط على زر العودة في الهاتف
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
